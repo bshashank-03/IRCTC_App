48eafe8a0ffec3abe66fb7276947a2b1ab048455
bshashank-03 <s.bhushan1807@gmail.com>
2025-04-06 17:35:31 +0530

    first commit

---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,47 @@
+package org.example.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserBookingService {
+
+    private User user;
+
+    private List<User> userList;
+
+    // USed for serialization and deserialization of json to text and vice versa
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "../localDb/users.json";
+
+    public UserBookingService(User user1) throws IOException {
+        this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>(){});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }
+        catch(IOException e){
+            return Boolean.FALSE;
+        }
+    }
+}
